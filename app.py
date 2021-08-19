e90b8f1f8e833e21f7a7e459b98695037f626365
Yacin <yacin.kharroubi@gmail.com>
2021-08-19 19:24:29 +0200

    Mise à jour du modèle de scoring

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def load_model(file_name):
 XGBoost_model = load_model("best_model_XGBoost_pickle.pkl")
 
 # Chargement des données SHAP
-# @st.cache(hash_funcs={'xgboost.sklearn.XGBClassifier': id})
+@st.cache(hash_funcs={'xgboost.sklearn.XGBClassifier': id})
 def load_shap(df, model):
     df_shap = df.iloc[:,1:-2]
     shap.initjs()
@@ -106,8 +106,13 @@ st.write("Statut du client :", target)
 
 
 #Préparation de la visualitation SHAP
-st.write(df.iloc[:,1:-2].shape)
-explainer_base_value, shap_values = load_shap(df, XGBoost_model)
+# explainer_base_value, shap_values = load_shap(df, XGBoost_model)
+df_shap = df.iloc[:,1:-2]
+shap.initjs()
+explainer = shap.TreeExplainer(XGBoost_model)
+explainer_base_value = round(explainer.expected_value[0],3)
+st.write(df_shap.shape)
+shap_values = explainer.shap_values(df_shap)
 # explainer = shap.TreeExplainer(XGBoost_model)
 
 # #On recupère les valeurs du client en fonction de l'ID selectionné
